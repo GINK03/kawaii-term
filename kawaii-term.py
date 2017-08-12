a7dd2368c76e52028fa9d11b98a242d75ead4a75
gimpei <gim.kobayashi@gmail.com>
2017-08-13 07:23:45 +0900

    a

--- kawaii-term.py
+++ kawaii-term.py
@@ -7,6 +7,25 @@ if '--mem' in sys.argv:
       line = line.strip()
       # デフォルトではKB表現だから、メガバイトで表現する
       mem  = re.split(r'\s{1,}', line)[-2]
-      mem  = '%d MB'%(int(mem)//1000)
+      mem  = 'Mem=%d MB'%(int(mem)//1000)
       print( mem )
       break
+
+if '--vmstat' in sys.argv:
+  buff = os.popen('vmstat').read().split('\n')
+  keys = re.split(r'\s{1,}', buff[1])
+  vals = re.split(r'\s{1,}', buff[2])
+  obj  = dict( zip(keys, vals) )
+  ucpu = '💻 ={0:03d}%'.format( int(obj['us']) )
+  print( ucpu )
+
+if '--disk' in sys.argv:
+  buff =os.popen('df -hT').read().split('\n')
+  # 末尾が/で終わるのが、ルートディレクトリ表示
+  keys = re.split(r'\s{1,}', buff[0])
+  vals = re.split(r'\s{1,}', list( filter(lambda x:len(x) != 0 and x[-1] == '/', buff[1:]) )[0] )
+  obj  = dict(zip( keys, vals) )
+  use  = obj['Use%']
+  free = obj['Avail']
+  result = 'DiskUse%={}, DiskAvail={}'.format(use, free)
+  print(result)
